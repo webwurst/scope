b1433c22506da90df524b106abba5cd0b623cd29
David Kaltschmidt <david.kaltschmidt@gmail.com>
2015-09-23 19:07:15 +0200

    render clustered nodes in a circle

--- client/app/scripts/charts/nodes-layout.js
+++ client/app/scripts/charts/nodes-layout.js
@@ -1,6 +1,6 @@
 const dagre = require('dagre');
 const debug = require('debug')('scope:nodes-layout');
-const Naming = require('../constants/naming');
+// const Naming = require('../constants/naming');
 const _ = require('lodash');
 
 const MAX_NODES = 100;
@@ -24,44 +24,73 @@ const doLayout = function(nodes, edges, width, height, scale, margins, topologyI
 
   // configure node margins
   graph.setGraph({
-    nodesep: scale(2.5),
-    ranksep: scale(2.5)
+    // rankdir: 'LR',
+    nodesep: scale(3),
+    ranksep: scale(5)
   });
 
-  // add nodes to the graph if not already there
-  _.each(nodes, function(node) {
-    if (!graph.hasNode(node.id)) {
-      graph.setNode(node.id, {
-        id: node.id,
-        width: scale(1),
-        height: scale(1)
+  // create rank nodes
+  const nodesByRank = _.groupBy(nodes, function(node) {
+    return node.rank;
+  });
+  const rankNodePrefix = 'scope-rank-';
+
+  _.each(nodesByRank, function(rankNodes, rank) {
+    if (rank) {
+      // nodes with rank are clustered in one big node in layout
+      const nodeId = rankNodePrefix + rank;
+      if (!graph.hasNode(nodeId)) {
+        graph.setNode(nodeId, {
+          id: nodeId,
+          width: scale(Math.sqrt(rankNodes.length)),
+          height: scale(Math.sqrt(rankNodes.length)),
+          nodes: rankNodes
+        });
+      }
+      // store rankNode reference in rank nodes
+      _.each(rankNodes, function(node) {
+        node.rankNode = nodeId;
+      });
+    } else {
+      // nodes with no rank get layed out individually
+      _.each(rankNodes, function(node) {
+        if (!graph.hasNode(node.id)) {
+          graph.setNode(node.id, {
+            id: node.id,
+            width: scale(1),
+            height: scale(1)
+          });
+        }
       });
     }
   });
 
+  // TODO remove rank and non-rank nodes
+
   // remove nodes that are no longer there
-  _.each(graph.nodes(), function(nodeid) {
-    if (!_.has(nodes, nodeid)) {
-      graph.removeNode(nodeid);
-    }
-  });
+  // _.each(graph.nodes(), function(nodeid) {
+  //   if (!_.has(nodes, nodeid)) {
+  //     graph.removeNode(nodeid);
+  //   }
+  // });
 
   // add edges to the graph if not already there
   _.each(edges, function(edge) {
-    if (!graph.hasEdge(edge.source.id, edge.target.id)) {
-      const virtualNodes = edge.source.id === edge.target.id ? 1 : 0;
-      graph.setEdge(edge.source.id, edge.target.id, {id: edge.id, minlen: virtualNodes});
+    const sourceId = edge.source.rankNode || edge.source.id;
+    const targetId = edge.target.rankNode || edge.target.id;
+    if (!graph.hasEdge(sourceId, targetId)) {
+      graph.setEdge(sourceId, targetId, {id: edge.id});
     }
   });
 
-  // remoed egdes that are no longer there
-  _.each(graph.edges(), function(edgeObj) {
-    const edge = [edgeObj.v, edgeObj.w];
-    const edgeId = edge.join(Naming.EDGE_ID_SEPARATOR);
-    if (!_.has(edges, edgeId)) {
-      graph.removeEdge(edgeObj.v, edgeObj.w);
-    }
-  });
+  // TODO remoed egdes that are no longer there
+  // _.each(graph.edges(), function(edgeObj) {
+  //   const edge = [edgeObj.v, edgeObj.w];
+  //   const edgeId = edge.join(Naming.EDGE_ID_SEPARATOR);
+  //   if (!_.has(edges, edgeId)) {
+  //     graph.removeEdge(edgeObj.v, edgeObj.w);
+  //   }
+  // });
 
   dagre.layout(graph);
 
@@ -79,23 +108,33 @@ const doLayout = function(nodes, edges, width, height, scale, margins, topologyI
   // apply coordinates to nodes and edges
 
   graph.nodes().forEach(function(id) {
-    const node = nodes[id];
     const graphNode = graph.node(id);
-    node.x = graphNode.x + offsetX;
-    node.y = graphNode.y + offsetY;
+    if (graphNode.nodes) {
+      // cluster node
+      const centerX = graphNode.x;
+      const centerY = graphNode.y;
+      const clusterCount = graphNode.nodes.length;
+      const radius = scale(Math.sqrt(clusterCount));
+      _.each(graphNode.nodes, function(node, i) {
+        const angle = Math.PI * 2 * i / clusterCount;
+        node.x = centerX + radius * Math.sin(angle);
+        node.y = centerY + radius * Math.cos(angle);
+      });
+    } else {
+      // normal node
+      const node = nodes[id];
+      node.x = graphNode.x + offsetX;
+      node.y = graphNode.y + offsetY;
+    }
   });
 
-  graph.edges().forEach(function(id) {
-    const graphEdge = graph.edge(id);
-    const edge = edges[graphEdge.id];
-    _.each(graphEdge.points, function(point) {
-      point.x += offsetX;
-      point.y += offsetY;
-    });
-    edge.points = graphEdge.points;
-    // set beginning and end points to node coordinates to ignore node bounding box
-    edge.points[0] = {x: edge.source.x, y: edge.source.y};
-    edge.points[edge.points.length - 1] = {x: edge.target.x, y: edge.target.y};
+  _.each(edges, function(edge) {
+    if (!edge.points) {
+      edge.points = [
+        {x: edge.source.x, y: edge.source.y},
+        {x: edge.target.x, y: edge.target.y}
+      ];
+    }
   });
 
   // return object with the width and height of layout
